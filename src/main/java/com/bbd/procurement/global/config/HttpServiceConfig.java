7d73dbcdaef24eff408ae68191266bc75d8f9ac7
dayoung0311 <dyl0311@gachon.ac.kr>
2026-06-17 18:41:25 +0900

    [Fix] : HttpServiceConfig 수정

--- src/main/java/com/bbd/procurement/global/config/HttpServiceConfig.java
+++ src/main/java/com/bbd/procurement/global/config/HttpServiceConfig.java
@@ -2,12 +2,7 @@ package com.bbd.procurement.global.config;
 
 import com.bbd.procurement.purchaseorder.adapter.out.external.ItemHttpService;
 import com.bbd.procurement.purchaseorder.adapter.out.external.SalesHttpService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.support.RestClientAdapter;
-import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import org.springframework.web.service.registry.ImportHttpServices;
 
 @Configuration
@@ -33,22 +28,4 @@ import org.springframework.web.service.registry.ImportHttpServices;
 )
 public class HttpServiceConfig {
 
-    @Bean
-    public ItemHttpService itemHttpService(@Value("${item.base-url}") String baseUrl) {
-        return proxyFactory(baseUrl).createClient(ItemHttpService.class);
-    }
-
-    @Bean
-    public SalesHttpService salesHttpService(@Value("${sales.base-url}") String baseUrl) {
-        return proxyFactory(baseUrl).createClient(SalesHttpService.class);
-    }
-
-    private HttpServiceProxyFactory proxyFactory(String baseUrl) {
-        RestClient restClient = RestClient.builder()
-                .baseUrl(baseUrl)
-                .build();
-        return HttpServiceProxyFactory
-                .builderFor(RestClientAdapter.create(restClient))
-                .build();
-    }
 }
